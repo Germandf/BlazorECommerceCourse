7a2c2955845a177154744690df967247990a2b58
German De Francesco <germandefrancesco@hotmail.com>
2022-03-22 21:18:08 -0300

    implemented EfCore with SqlServer

--- BlazorECommerceCourse/Server/Program.cs
+++ BlazorECommerceCourse/Server/Program.cs
@@ -1,9 +1,15 @@
+global using BlazorECommerceCourse.Server.Data;
 global using BlazorECommerceCourse.Shared;
+global using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
