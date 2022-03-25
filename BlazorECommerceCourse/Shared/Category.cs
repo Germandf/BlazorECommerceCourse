3e131e59d1275264e2d627f62fb79a6171caf334
German De Francesco <germandefrancesco@hotmail.com>
2022-03-25 18:53:02 -0300

    added Categories in ef

---
+++ BlazorECommerceCourse/Shared/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorECommerceCourse.Shared;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Url { get; set; } = null!;
+
+}
