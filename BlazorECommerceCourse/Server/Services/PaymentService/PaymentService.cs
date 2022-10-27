3b8c803c50fd9b250e2759b20335d09711b7b74b
German De Francesco <germandefrancesco@hotmail.com>
2022-10-26 22:51:20 -0300

    Add address requirement in Stripe form

--- BlazorECommerceCourse/Server/Services/PaymentService/PaymentService.cs
+++ BlazorECommerceCourse/Server/Services/PaymentService/PaymentService.cs
@@ -45,6 +45,10 @@ public class PaymentService : IPaymentService
         var options = new SessionCreateOptions()
         {
             CustomerEmail = _authService.GetUserEmail(),
+            ShippingAddressCollection = new SessionShippingAddressCollectionOptions()
+            {
+                AllowedCountries = new List<string>() { "AR" }
+            },
             PaymentMethodTypes = new List<string>()
             {
                 "card"
